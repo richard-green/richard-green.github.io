49374e9af31bc3d00c019bebd18aaf986e77863f
Richard Green <rich@rdgreen.dev>
2025-09-02 18:16:34 +0100

    JSON style updates

--- js/jsonHighlight.js
+++ js/jsonHighlight.js
@@ -1,30 +1,58 @@
 var jsonHighlight = (function () {
+  const pattern =
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g;
 
-	function highlight(json) {
-	    if (typeof json !== 'string') {
-	        json = JSON.stringify(json, undefined, 2);
-            }
-	    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-	        var cls = 'number';
-	        if (/^"/.test(match)) {
-	            if (/:$/.test(match)) {
-	                cls = 'key';
-	            } else {
-	                cls = 'string';
-	            }
-	        } else if (/true|false/.test(match)) {
-	            cls = 'boolean';
-	        } else if (/null/.test(match)) {
-	            cls = 'null';
-	        }
-	        return '<span class="' + cls + '">' + match + '</span>';
-	    });
-	}
+  function getMatches(data) {
+    let json = JSON.stringify(data, undefined, 2);
+    let matches = [];
+    let lastIndex = 0;
+    let match;
+    // Use regex exec to capture match positions.
+    while ((match = pattern.exec(json)) !== null) {
+      // Capture text between matches as plain text.
+      if (match.index > lastIndex) {
+        matches.push({
+          text: json.slice(lastIndex, match.index),
+          cls: "plain",
+        });
+      }
+      let text = match[0];
+      // Determine css class based on the match.
+      let cls = getClass(text);
+      if (cls === "key") {
+        text = text.substring(0, text.length - 1);
+        matches.push({ text, cls });
+        matches.push({ text: ":", cls: "plain" });
+      } else {
+        matches.push({ text, cls });
+      }
+      lastIndex = pattern.lastIndex;
+    }
+    // Capture any remaining text after the last match.
+    if (lastIndex < json.length) {
+      matches.push({ text: json.slice(lastIndex), cls: "plain" });
+    }
+    return matches;
+  }
 
-	// Public methods
-	return {
-		highlight: highlight
-	};
+  function getClass(match) {
+    var cls = "number";
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+        cls = "key";
+      } else {
+        cls = "string";
+      }
+    } else if (/true|false/.test(match)) {
+      cls = "boolean";
+    } else if (/null/.test(match)) {
+      cls = "null";
+    }
+    return cls;
+  }
 
+  // Public methods
+  return {
+    getMatches: getMatches,
+  };
 })();
